ca3055493efc5bdb46e6f0db9c9c10a454417c3e
Rafael <rafael_guliev94@list.ru>
2023-02-20 18:48:48 +0300

    fix start script

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ const port = 3000
 
 executeCode(app);
 
+app.get('/', (req, res) => {
+    res.send('Codelearn server is running.');
+    res.status(200);
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
