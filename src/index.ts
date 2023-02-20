d0305f84db438e31b4b8fd98381d547daea44a2f
Rafael <rafael_guliev94@list.ru>
2023-02-20 18:24:59 +0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import { executeCode } from './services/execCode/executeCode';
+const app = express()
+app.use(cors(), express.json());
+const port = 3000
+
+executeCode(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
