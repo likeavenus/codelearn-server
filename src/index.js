dd84dd2b5fadad9123e6e2fc680ae320883fd875
Rafael <rafael_guliev94@list.ru>
2023-02-20 18:59:51 +0300

    remove ts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var cors_1 = require("cors");
-var executeCode_1 = require("./services/execCode/executeCode");
-var app = (0, express_1["default"])();
-app.use((0, cors_1["default"])(), express_1["default"].json());
-var port = 3000;
-(0, executeCode_1.executeCode)(app);
-app.listen(port, function () {
-    console.log("Example app listening on port ".concat(port));
+const express = require('express');
+const cors = require('cors');
+const executeCode = require('./services/execCode/executeCode');
+
+const app = express()
+app.use(cors(), express.json());
+const port = 3000
+
+executeCode(app);
+
+app.get('/', (req, res) => {
+    res.send('Codelearn server is running.');
+    res.status(200);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 });
--- src/services/execCode/executeCode.js
+++ src/services/execCode/executeCode.js
@@ -1,24 +1,22 @@
-"use strict";
-exports.__esModule = true;
-exports.executeCode = void 0;
-var vm2_1 = require("vm2");
+const VM = require('vm2');
+
 function executeCode(app) {
-    app.post('/run-code', function (req, res) {
-        var body = req.body;
-        var vm = new vm2_1.VM({
-            wasm: true
+    app.post('/run-code', (req, res) => {
+        const { body } = req;
+        const vm = new VM({
+            wasm: true,
         });
         try {
-            if (!body.code)
-                return;
-            var result = vm.run(body.code);
+            if (!body.code) return;
+            const result = vm.run(body.code);
             if (result) {
                 res.status(200).json(result);
             }
-        }
-        catch (e) {
+
+        } catch (e) {
             res.status(500).json(e.message);
         }
     });
 }
-exports.executeCode = executeCode;
+
+module.exports = executeCode;
