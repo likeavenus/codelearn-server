ef6e828baa4711f595e146f74d42ff7a58e5df6d
Rafael <rafael_guliev94@list.ru>
2023-02-20 19:13:39 +0300

    add api folder

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const executeCode = require('./services/execCode/executeCode');
+const executeCode = require('../services/execCode/executeCode');
 
 const app = express()
 app.use(cors(), express.json());
