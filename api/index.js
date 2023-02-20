e33d58c446e8e9557e06e7d4605488693c190370
Rafael <rafael_guliev94@list.ru>
2023-02-20 19:39:55 +0300

    cors

--- api/index.js
+++ api/index.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const VM = require('vm2');
 const cors = require('cors');
-const executeCode = require('../services/execCode/executeCode');
 
 const app = express()
 app.use(cors(), express.json());
 const port = 3000
 
-executeCode(app);
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 app.get('/', (req, res) => {
     res.send('Codelearn server is running.');
     res.status(200);
-})
+});
+
+app.post('/run-code', (req, res) => {
+    const { body } = req;
+    const vm = new VM({
+        wasm: true,
+    });
+    try {
+        if (!body.code) return;
+        const result = vm.run(body.code);
+        if (result) {
+            res.status(200).json(result);
+        }
+
+    } catch (e) {
+        res.status(500).json(e.message);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
