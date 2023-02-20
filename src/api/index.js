07988a23aac914169dc5c346fdbccde4d5946fc4
Rafael <rafael_guliev94@list.ru>
2023-02-20 19:31:34 +0300

    cors

--- src/api/index.js
+++ src/api/index.js
@@ -12,6 +12,11 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 app.get('/', (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     res.send('Codelearn server is running.');
     res.status(200);
 })
